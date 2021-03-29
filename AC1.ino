e56bc13ca37455237efa01e3c18d6ee45ba461f4
lourenne <78864076+lourenne@users.noreply.github.com>
2021-03-28 22:29:36 -0300

    colocando o segundo botão

--- AC1.ino
+++ AC1.ino
@@ -3,3 +3,16 @@ const int vermelho = 5;
 const int verde = 6;
 const int azul = 7;
 // colocando o ponto e virgula
+
+
+//colocando o segundo botão
+const int botao1 = 2;
+const int botao2 = 3;
+unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+const int botaoDelay = 100;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
